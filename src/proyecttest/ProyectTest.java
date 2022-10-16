5a58bc945f59cf8df56084413532d40a04b6d9a2
Diego Díaz <kbyteec@gmail.com>
2022-10-16 01:44:22 -0500

    Initialized

--- src/proyecttest/ProyectTest.java
+++ src/proyecttest/ProyectTest.java
@@ -15,6 +15,6 @@ public class ProyectTest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-    }
+        System.out.println("Hi World");    }
     
 }
